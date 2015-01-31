9cb6d6eb2bd395922fdaa629e9263c9224ac5b9d
Wes Turner <wes@wrd.nu>
2015-01-31 14:12:34 -0600

    BLD: build.sh: generate rough schemas/index.html and ns/index.html

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash +e +x
 
+# Generate JSON-LD @contexts from OpenFDA ElasticSearch JSON mappings
+# NOTE: this script expects to be run within the repo root directory
+
 git clone https://github.com/westurner/elasticsearchjsonld
 pip install -e elasticsearchjsonld/
 ./elasticsearchjsonld/scripts/build_openfda_jsonld_contexts.sh ./schemas ./ns
@@ -7,3 +10,14 @@ pip install -e elasticsearchjsonld/
 echo "<html><head><title>openfda-jsonld-testing</title></head><body><pre>" > index.html
 find ./schemas ./ns | tr '<' '_' | tr '>' '_' >> index.html  # XXX
 echo "</pre></body></html>" >> index.html
+
+
+(cd ./schemas && \
+echo "<html><head><title>openfda-jsonld-testing</title></head><body><pre>" > index.html && \
+(find . | tr '<' '_' | tr '>' '_' >> index.html)  &&
+echo "</pre></body></html>" >> index.html)
+
+(cd ./ns && \
+echo "<html><head><title>openfda-jsonld-testing</title></head><body><pre>" > index.html && \
+(find . | tr '<' '_' | tr '>' '_' >> index.html)  &&
+echo "</pre></body></html>" >> index.html)
